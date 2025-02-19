4b3aba97f6e68a3ae7364a4134d310dcd91d112d
Tahani Alotaibi <79321400+TMQ5@users.noreply.github.com>
2025-02-19 20:41:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,21 +56,21 @@ fig, axes = plt.subplots(2, 2, figsize=(14, 10))
 
 # 1️⃣ عدد الشقق في كل حي
 axes[0, 0].set_title(get_display(arabic_reshaper.reshape("ما هي الأحياء التي تحتوي على أكبر عدد من الشقق؟")))
-sns.barplot(y=top_districts_apartments['الحي'], x=top_districts_apartments['count'], palette="mako", orient='h', ax=axes[0, 0])
+sns.barplot(y=[get_display(arabic_reshaper.reshape(label)) for label in top_districts_apartments['الحي']], x=top_districts_apartments['count'], palette="mako", orient='h', ax=axes[0, 0])
 axes[0, 0].set_xlabel(get_display(arabic_reshaper.reshape("عدد الشقق")))
 axes[0, 0].set_ylabel(get_display(arabic_reshaper.reshape("الحي")))
 axes[0, 0].invert_yaxis()
 
 # 2️⃣ عدد الفلل في كل حي
 axes[0, 1].set_title(get_display(arabic_reshaper.reshape("ما هي الأحياء التي تحتوي على أكبر عدد من الفلل؟")))
-sns.barplot(y=top_districts_villas['الحي'], x=top_districts_villas['count'], palette="mako", orient='h', ax=axes[0, 1])
+sns.barplot(y=[get_display(arabic_reshaper.reshape(label)) for label in top_districts_villas['الحي']], x=top_districts_villas['count'], palette="mako", orient='h', ax=axes[0, 1])
 axes[0, 1].set_xlabel(get_display(arabic_reshaper.reshape("عدد الفلل")))
 axes[0, 1].set_ylabel(get_display(arabic_reshaper.reshape("الحي")))
 axes[0, 1].invert_yaxis()
 
 # 3️⃣ متوسط السعر الإجمالي للشقق
 axes[1, 0].set_title(get_display(arabic_reshaper.reshape("ما هي الأحياء الأقل سعراً في متوسط السعر الإجمالي للشقق؟")))
-sns.barplot(y=top_cheapest_districts_apartments['الحي'], x=top_cheapest_districts_apartments['السعر الاجمالي'], palette="mako", orient='h', ax=axes[1, 0])
+sns.barplot(y=[get_display(arabic_reshaper.reshape(label)) for label in top_cheapest_districts_apartments['الحي']], x=top_cheapest_districts_apartments['السعر الاجمالي'], palette="mako", orient='h', ax=axes[1, 0])
 axes[1, 0].set_xlabel(get_display(arabic_reshaper.reshape("متوسط السعر الإجمالي")))
 axes[1, 0].set_ylabel(get_display(arabic_reshaper.reshape("الحي")))
 axes[1, 0].invert_yaxis()
@@ -78,7 +78,7 @@ axes[1, 0].xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f'{int(x)
 
 # 4️⃣ متوسط السعر الإجمالي للفلل
 axes[1, 1].set_title(get_display(arabic_reshaper.reshape("ما هي الأحياء الأقل سعراً في متوسط السعر الإجمالي للفلل؟")))
-sns.barplot(y=top_cheapest_districts_villas['الحي'], x=top_cheapest_districts_villas['السعر الاجمالي'], palette="mako", orient='h', ax=axes[1, 1])
+sns.barplot(y=[get_display(arabic_reshaper.reshape(label)) for label in top_cheapest_districts_villas['الحي']], x=top_cheapest_districts_villas['السعر الاجمالي'], palette="mako", orient='h', ax=axes[1, 1])
 axes[1, 1].set_xlabel(get_display(arabic_reshaper.reshape("متوسط السعر الإجمالي")))
 axes[1, 1].set_ylabel(get_display(arabic_reshaper.reshape("الحي")))
 axes[1, 1].invert_yaxis()
